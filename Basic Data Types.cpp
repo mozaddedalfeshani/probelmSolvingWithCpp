988ed6a0472a61e4a2a3ffbda529eafe2296ef1e
mozaddedalfeshani <mozaddedalfeshanimurad@gmail.com>
2022-08-12 14:22:50 +0600

    basic data types

---
+++ Basic Data Types.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <math.h>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a;
+    long int b;
+    char ch;
+    float c;
+    double fl;
+
+    cin >> a >> b >> ch >> c >> fl;
+    cout << a << endl;
+    cout << b << endl;
+    cout << ch << endl;
+    cout << fixed << setprecision(3) << c << endl;
+    cout << fixed << setprecision(9) << fl << endl;
+
+    return 0;
+}
