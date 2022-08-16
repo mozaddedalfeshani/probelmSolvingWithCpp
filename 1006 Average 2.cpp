9669fd085b718600252fa1d97fb35bdc1133b594
mozaddedalfeshani <mozaddedalfeshanimurad@gmail.com>
2022-08-17 00:13:02 +0600

    none

---
+++ 1006 Average 2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    double a, b, c, avg;
+    cin >> a >> b >> c;
+
+    return 0;
+}
---
+++ 2749 - Output 3.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+
+    printf("---------------------------------------\n");
+    printf("|x = 35                               |\n");
+    printf("|                                     |\n");
+    printf("|               x = 35                |\n");
+    printf("|                                     |\n");
+    printf("|                               x = 35|\n");
+    printf("---------------------------------------\n");
+
+    return 0;
+}
