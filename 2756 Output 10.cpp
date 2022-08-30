af21825ea5738cc33881ad39e1d5cbc87a710994
mozaddedalfeshani <mozaddedalfeshanimurad@gmail.com>
2022-08-30 17:49:53 +0600

    2756 Solved

---
+++ 2756 Output 10.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    printf("       A\n");
+    printf("      B B\n");
+    printf("     C   C\n");
+    printf("    D     D\n");
+    printf("   E       E\n");
+    printf("    D     D\n");
+    printf("     C   C\n");
+    printf("      B B\n");
+    printf("       A\n");
+    return 0;
+}
