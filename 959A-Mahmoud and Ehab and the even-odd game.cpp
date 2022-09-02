1f3b7a366dd2553fd9a3493fa697990da6b144aa
mozaddedalfeshani <mozaddedalfeshanimurad@gmail.com>
2022-09-02 23:44:35 +0600

    If-Else

---
+++ 959A-Mahmoud and Ehab and the even-odd game.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int input;
+    cin >> input;
+    if (input % 2 == 0)
+    {
+        cout << "Mahmoud" << endl;
+    }
+    else
+    {
+        cout << "Ehab" << endl;
+    }
+    return 0;
+}
