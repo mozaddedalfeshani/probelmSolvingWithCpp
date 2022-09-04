773ddad3fca13780435769fa79a8b3a656b67a4c
mozaddedalfeshani <mozaddedalfeshanimurad@gmail.com>
2022-09-04 14:57:21 +0600

    simple

---
+++ 1050 DDD.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main()
+
+{
+    int input;
+    cin >> input;
+    if (input == 61)
+    {
+        cout << "Brasilia" << endl;
+    }
+    else if (input == 71)
+    {
+        cout << "Salvador" << endl;
+    }
+    else if (input == 11)
+    {
+        cout << "Sao Paulo" << endl;
+    }
+    else if (input == 21)
+    {
+        cout << "Rio de Janeiro" << endl;
+    }
+    else if (input == 32)
+    {
+        cout << "Juiz de Fora" << endl;
+    }
+    else if (input == 19)
+    {
+        cout << "Campinas" << endl;
+    }
+    else if (input == 27)
+    {
+        cout << "Vitoria" << endl;
+    }
+    else if (input == 310)
+    {
+        cout << "Belo Horizonte" << endl;
+    }
+    else
+    {
+        cout << "DDD nao cadastrado" << endl;
+    }
+    return 0;
+}
