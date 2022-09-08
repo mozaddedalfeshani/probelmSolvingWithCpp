2ade7823f47a2727193076165d53483866faa15a
mozaddedalfeshani <mozaddedalfeshanimurad@gmail.com>
2022-09-09 02:46:34 +0600

    none

--- 1132 Multiples of 13.cpp
+++ 1132 Multiples of 13.cpp
@@ -1,18 +1,43 @@
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int first, last, sum = 0;
+
+//     cin >> first >> last;
+//     for (short int i = first; i <= last; i++)
+//     {
+//         if (i % 13 == 0)
+//         {
+//             sum = sum + i;
+//         }
+//     }
+//     cout << sum << endl;
+
+//     return 0;
+// }
+
+//
 #include <iostream>
 using namespace std;
 int main()
 {
-    int first, last, sum = 0;
+    int x, y, aux;
+    int soma = 0;
+    cin >> x >> y;
 
-    cin >> first >> last;
-    for (short int i = first; i <= last; i++)
+    if (x > y)
     {
-        if (i % 13 == 0)
-        {
-            sum = sum + i;
-        }
+        aux = y;
+        y = x;
+        x = aux;
     }
-    cout << sum << endl;
 
+    for (int i = x; i <= y; i++)
+    {
+        if (i % 13 != 0)
+            soma += i;
+    }
+    cout << soma << endl;
     return 0;
 }
