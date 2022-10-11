e2664c33a3629cf8907c6f21c27a5878762317e7
mozaddedalfeshani <mozaddedalfeshanimurad@gmail.com>
2022-10-11 19:56:13 +0600

    modified:   9. Palindrome Number.cpp

--- 9. Palindrome Number.cpp
+++ 9. Palindrome Number.cpp
@@ -1,3 +1,25 @@
+//this is leetcode solution
+class Solution
+{
+public:
+    bool isPalindrome(int x)
+    {
+        string s = to_string(x);
+        int start = 0;
+        int end = s.length() - 1;
+
+        while (start < end)
+        {
+            if (s[start++] != s[end--])
+                return false;
+        }
+
+        return true;
+    }
+};
+
+//normal solution for understant deeply
+
 #include <iostream>
 using namespace std;
 int main()
