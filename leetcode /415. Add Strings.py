c8a31c17d747ef5d3082d9428d53f1679b39ea1d
mozaddedalfeshani <mozaddedalfeshanimurad@gmail.com>
2023-04-03 21:03:46 +0600

    adding nothin

---
+++ leetcode /415. Add Strings.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def addStrings(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        answer = int(num1) + int(num2)
+        return str(answer)
