bd20bd402fd0a6cd8c2753ebe4ffe06fe0c60a75
mozaddedalfeshani <mozaddedalfeshanimurad@gmail.com>
2023-10-16 23:03:07 +0600

    solved

---
+++ p24q4.cpp
@@ -0,0 +1,12 @@
+//Value of nearest Root integer
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+  double value;
+  cout<< "Enter the value : "<<endl;
+  cin>> value;
+  int result = sqrt(value);
+  cout<< result<<endl;
+  return 0;
+}
